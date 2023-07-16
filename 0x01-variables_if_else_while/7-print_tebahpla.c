a22612e2ba2903a1c0750a4573dd33cd4444be2a
Yohanna Bet Hub <yohannamerawi@gmail.com>
2023-07-16 12:42:12 -0700

    git commit -m 7-print_tebahpla.c 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char d;
+
+	d = 'z';
+	while
+		(d >= 'a') {
+			putchar(d);
+			d--;
+		}
+	putchar('\n');
+	return (0);
+}
