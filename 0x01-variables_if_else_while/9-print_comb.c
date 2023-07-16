3061e63ff3ed514e12cabb140111653fdb361f69
Yohanna Bet Hub <yohannamerawi@gmail.com>
2023-07-16 13:03:57 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+
+	while
+		(num <= 9) {
+			putchar(num + '0');
+			putchar(',');
+			putchar(' ');
+			num++;
+		}
+	putchar('\n');
+	return (0);
+}
