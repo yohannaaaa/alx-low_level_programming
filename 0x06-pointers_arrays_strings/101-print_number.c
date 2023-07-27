e573de9689f325befd501cc0cdde07d3883644c5
Yohanna Bet Hub <yohannamerawi@gmail.com>
2023-07-27 11:25:43 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: integer parms
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int i;
+
+	i = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -n;
+	}
+	if (i / 10 != 0)
+	{
+		print_number(i / 10);
+	}
+	_putchar((i % 10) + '0');
+}
