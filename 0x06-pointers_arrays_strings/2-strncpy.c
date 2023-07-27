f8773ff491ae82540c7f6eed201433fdeb44f48b
Yohanna Bet Hub <yohannamerawi@gmail.com>
2023-07-27 09:11:44 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-	for (j = 0; j < n && src[j] != '\0'; j++)
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
 		dest[j] = src[j];
+		j++;
 	}
-	for (j = 0, j < n; dest[j] = '\0'; j++)
+	while (j < n)
 	{
+		 dest[j] = '\0';
+		 j++;
 	}
 	return (dest);
 }
