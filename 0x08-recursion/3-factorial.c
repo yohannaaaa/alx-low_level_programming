119411dd1c836d267385a3b5a15e3a867c4c5975
Yohanna Bet Hub <yohannamerawi@gmail.com>
2023-08-02 00:56:44 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,13 +10,9 @@ int factorial(int n)
 	int i;
 	int res = 1;
 
-	if (n == 0 || n == 1)
-	{
-		return (1);
-	}
-	for (i = 2; i <= n; i++)
-	{
-		res *= i;
-	}
-	return (res);
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (-1);
+	return (n * factorial(n - 1));
 }
