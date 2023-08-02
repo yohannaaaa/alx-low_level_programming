b31cd709d021cd76848af6ac1bbcf33b1dc15171
Yohanna Bet Hub <yohannamerawi@gmail.com>
2023-08-02 00:40:53 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main,h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: The string to be measured
+ * Return: The lenth of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+	}
+	return (i);
+}
