3210772617e9604629bbad9aab51935888e54afa
Yohanna Bet Hub <yohannamerawi@gmail.com>
2023-08-04 04:37:57 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
@@ -10,7 +11,7 @@
 int check_num(char *str)
 {
 	/*Declaring variables*/
-	unsigned int count;
+	unsigned int c;
 
 	c = 0;
 	while (c < strlen(str)) /*count string*/
@@ -45,18 +46,20 @@ int main(int argc, char *argv[])
 		if (check_num(argv[c]))
 
 		{
-			str_to_int = atoi(atgv[c]); /*ATOI --> convert string to int*/
+			str_to_int = atoi(argv[c]); /*ATOI --> convert string to int*/
 			sum += str_to_int;
 		}
 
 		/*other condition if the number contains symbols that are not digits*/
 		else
-		{printf("Error\n");
+		{
+			printf("Error\n");
 			return (1);
 		}
 
 		c++;
 	}
+
 	printf("%d\n", sum); /*prints sum*/
 
 	return (0);
