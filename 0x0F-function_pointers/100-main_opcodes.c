ed4030f209d8b8a5b71e5a85826fc0eb63e101ad
Yohanna Bet Hub <yohannamerawi@gmail.com>
2023-08-16 14:08:07 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int b, i;
+	int bytes, i;
 	char *arr;
 
 	if (argc != 2)
@@ -17,22 +17,22 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	b = atoi(argv[1]);
+	bytes = atoi(argv[1]);
 
-	if (b < 0)
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 	arr = (char *)main;
-	for (i = 0; i < b; i++)
+	for (i = 0; i < bytes; i++)
 	{
-		if (i == b - 1)
+		if (i == bytes - 1)
 		{
 			printf("%02hhx\n", arr[i]);
 			break;
 		}
-		printf("%02hhx", arr[i]);
+		printf("%02hhx ", arr[i]);
 	}
 	return (0);
 }
