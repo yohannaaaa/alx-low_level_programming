b925193a88509e6634ae9681c532b26151ef29ba
Yohanna Bet Hub <yohannamerawi@gmail.com>
2023-08-17 13:20:36 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,8 +19,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(nums, int));
 		if (i != (n - 1) && separator != NULL)
-			printf("%s", separator != NULL);
+			printf("%s", separator);
 	}
-	prinf("\n");
+	printf("\n");
 	va_end(nums);
 }
